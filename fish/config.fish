1c032bdf20e93ec932f222071959b1820dbeb9ce
Seahal <shiiharagiken@gmail.com>
2019-02-12 21:02:37 +0900

    Update: Fish and tmux settings was updated.

--- fish/config.fish
+++ fish/config.fish
@@ -31,7 +31,7 @@ set -g theme_show_exit_status yes
 set -g theme_display_cmd_duration yes
 set -g theme_display_git_ahead_verbose yes
 set -g theme_display_git_dirty_verbose yes
-set -g theme_display_git_master_branch yes
+set -g theme_display_git_master_branch no
 set -g theme_git_worktree_support yes
 set -g theme_title_display_process yes
 set -g theme_color_scheme terminal2
